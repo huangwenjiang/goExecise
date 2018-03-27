96eda411b2927789fe8a6c5d2054f37e58ae8a12
huangwenjiang <wehuang@WEHUANG-e7450.internal.synopsys.com>
2018-03-27 18:01:47 +0800

    execise1.4

---
+++ execise1/execise1.4/dup.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type filenames []string
+
+func main() {
+	counts := make(map[string]filenames)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, "STDOUT")
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2:%v", err)
+			}
+			countLines(f, counts, arg)
+		}
+	}
+	for line, n := range counts {
+		if len(n) > 1 {
+			fmt.Printf("%v\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]filenames, filename string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		v := counts[input.Text()]
+		counts[input.Text()] = append(v, filename)
+	}
+}
